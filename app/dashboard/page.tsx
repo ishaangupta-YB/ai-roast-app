951e11d5154f7b7e034ce31773d177f77b2f3c25
Ishaan Gupta <ishaangupta999@gmail.com>
2024-07-14 02:50:24 +0530

    added basic UI

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
+import PlaceholderContent from "@/components/DashBoardLayout/placeholder-content";
+import { ContentLayout } from "@/components/DashBoardLayout/content-layout";
+
+export default function DashboardPage() {
+  return (
+    <ContentLayout title="Dashboard">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Dashboard</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <PlaceholderContent />
+    </ContentLayout>
+  );
+}
